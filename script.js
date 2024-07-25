07de9c084c8e2832ce291180241881d14538d98e
Loïc Steinmetz <loic.steinmetz@jamespot.com>
2024-07-25 15:28:34 +0200

    Up

--- script.js
+++ script.js
@@ -1,23 +1,97 @@
-const bg = document.querySelector('#bg');
-const nbRays = 300;
-
-[...Array(nbRays).keys()].forEach(_ => {
-    const div = document.createElement('div');
-    div.classList.add('ray');
-    bg.appendChild(div);
-});
-
+const getSize = () => 200 - Math.floor(Math.random() * 150);
+const getX = (size) => window.innerWidth - size / 2 - Math.floor(Math.random() * (window.innerWidth));
+const getY = (size) => window.innerHeight - size / 2 - Math.floor(Math.random() * (window.innerHeight));
 const shuffle = () => {
-    document.querySelectorAll('.ray').forEach(r => {
-        r.style.width = Math.floor(Math.random() * 75) + '%';
-        r.style.marginLeft = Math.floor(Math.random() * 60) + '%';
-        const c = 256 - Math.random() * 50;
-        r.style.backgroundColor = `rgb(${c}, ${c}, ${c})`;
-    });
+    const circle1 = document.querySelector('#circle1');
+    const circle2 = document.querySelector('#circle2');
+    const square1 = document.querySelector('#square1');
+    const square2 = document.querySelector('#square2');
+
+    const circle1Size = getSize();
+    circle1.style.width = circle1Size + 'px';
+    circle1.style.left = getX(circle1Size) + 'px';
+    circle1.style.top = getY(circle1Size) + 'px';
+
+    const circle2Size = getSize();
+    circle2.style.width = circle2Size + 'px';
+    circle2.style.left = getX(circle2Size) + 'px';
+    circle2.style.top = getY(circle2Size) + 'px';
+
+    const square1Size = getSize();
+    square1.style.width = square1Size + 'px';
+    square1.style.left = getX(square1Size) + 'px';
+    square1.style.top = getY(square1Size) + 'px';
+
+    const square2Size = getSize();
+    square2.style.width = square2Size + 'px';
+    square2.style.left = getX(square2Size) + 'px';
+    square2.style.top = getY(square2Size) + 'px';
 }
 
-setInterval(shuffle, 4000);
-setTimeout(shuffle, 100);
+
+(() => {
+    const texts = document.querySelectorAll('p');
+    let i = 0;
+    const next = () => i = i < texts.length - 1 ? i + 1 : 0;
+    const prev = () => i = i > 0 ? i - 1 : texts.length - 1;
+    document.addEventListener('mousedown', () => {
+        console.log(i)
+        texts[i].style.display = 'none';
+    });
+    document.addEventListener('mouseup', (e) => {
+        shuffle();
+        if (e.clientX < window.innerWidth / 2) {
+            prev();
+        } else {
+            next();
+        }
+        texts[i].style.display = 'block';
+    });
+})();
+
+(() => {
+    const container = document.querySelector('main');
+
+    const circle1 = document.createElement('div');
+    circle1.id = 'circle1';
+    circle1.classList.add('shape');
+    const circle1Size = getSize();
+    circle1.style.borderRadius = '100%';
+    circle1.style.width = circle1Size + 'px';
+    circle1.style.left = getX(circle1Size) + 'px';
+    circle1.style.top = getY(circle1Size) + 'px';
+    container.appendChild(circle1);
+
+    const circle2 = document.createElement('div');
+    circle2.id = 'circle2';
+    circle2.classList.add('shape');
+    const circle2Size = getSize();
+    circle2.style.borderRadius = '100%';
+    circle2.style.width = circle2Size + 'px';
+    circle2.style.left = getX(circle2Size) + 'px';
+    circle2.style.top = getY(circle2Size) + 'px';
+    container.appendChild(circle2);
+
+    const square1 = document.createElement('div');
+    square1.id = 'square1';
+    square1.classList.add('shape');
+    const square1Size = getSize();
+    square1.style.width = square1Size + 'px';
+    square1.style.left = getX(square1Size) + 'px';
+    square1.style.top = getY(square1Size) + 'px';
+    container.appendChild(square1);
+
+    const square2 = document.createElement('div');
+    square2.id = 'square2';
+    square2.classList.add('shape');
+    const square2Size = getSize();
+    square2.style.width = square2Size + 'px';
+    square2.style.left = getX(square2Size) + 'px';
+    square2.style.top = getY(square2Size) + 'px';
+    container.appendChild(square2);
+})();
+
+/* Styles */
 
 document.querySelectorAll('.wave').forEach(w => {
     const t = w.innerHTML;
@@ -46,22 +120,4 @@ document.querySelectorAll('.i-blink').forEach(w => {
         w.innerHTML = letter[i];
         i = i < letter.length - 1 ? i + 1 : 0;
     }, 200)
-});
-    
-
-const texts = document.querySelectorAll('p');
-let j = 0;
-const next = () => j = j < texts.length - 1 ? j + 1 : 0;
-const prev = () => j = j > 0 ? j - 1 : texts.length - 1;
-document.addEventListener('mousedown', () => {
-    console.log(j)
-    texts[j].style.display = 'none';
-});
-document.addEventListener('mouseup', (e) => {
-    if (e.clientX < window.innerWidth / 2) {
-        prev();
-    } else {
-        next();
-    }
-    texts[j].style.display = 'block';
 });
