ac3705405154130eab4cdade8e6c5dbb5dea2002
Loïc Steinmetz <loic.steinmetz@jamespot.com>
2024-07-25 17:03:32 +0200

    Up

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const getSize = () => 200 - Math.floor(Math.random() * 150);
 const getX = (size) => window.innerWidth - size / 2 - Math.floor(Math.random() * (window.innerWidth));
 const getY = (size) => window.innerHeight - size / 2 - Math.floor(Math.random() * (window.innerHeight));
+const getRotation = () => Math.floor(Math.random() * 180);
+
 const shuffle = () => {
     const circle1 = document.querySelector('#circle1');
     const circle2 = document.querySelector('#circle2');
@@ -11,11 +13,13 @@ const shuffle = () => {
     circle1.style.width = circle1Size + 'px';
     circle1.style.left = getX(circle1Size) + 'px';
     circle1.style.top = getY(circle1Size) + 'px';
+    circle1.style.transform = `rotate(${getRotation()}deg)`
 
     const circle2Size = getSize();
     circle2.style.width = circle2Size + 'px';
     circle2.style.left = getX(circle2Size) + 'px';
     circle2.style.top = getY(circle2Size) + 'px';
+    circle2.style.transform = `rotate(${getRotation()}deg)`
 
     const square1X = getSize();
     const square1Y = getSize();
@@ -23,6 +27,7 @@ const shuffle = () => {
     square1.style.height = square1Y + 'px';
     square1.style.left = getX(square1X) + 'px';
     square1.style.top = getY(square1Y) + 'px';
+    square1.style.transform = `rotate(${getRotation()}deg)`
 
     const square2X = getSize();
     const square2Y = getSize();
@@ -30,6 +35,7 @@ const shuffle = () => {
     square2.style.height = square2Y + 'px';
     square2.style.left = getX(square2X) + 'px';
     square2.style.top = getY(square2Y) + 'px';
+    square2.style.transform = `rotate(${getRotation()}deg)`
 }
 
 
@@ -65,6 +71,7 @@ const shuffle = () => {
     circle1.style.width = circle1Size + 'px';
     circle1.style.left = getX(circle1Size) + 'px';
     circle1.style.top = getY(circle1Size) + 'px';
+    circle1.style.transform = `rotate(${getRotation()}deg)`
     container.appendChild(circle1);
 
     const circle2 = document.createElement('div');
@@ -76,6 +83,7 @@ const shuffle = () => {
     circle2.style.width = circle2Size + 'px';
     circle2.style.left = getX(circle2Size) + 'px';
     circle2.style.top = getY(circle2Size) + 'px';
+    circle2.style.transform = `rotate(${getRotation()}deg)`
     container.appendChild(circle2);
 
     const square1 = document.createElement('div');
@@ -87,6 +95,7 @@ const shuffle = () => {
     square1.style.height = square1Y + 'px';
     square1.style.left = getX(square1X) + 'px';
     square1.style.top = getY(square1Y) + 'px';
+    square1.style.transform = `rotate(${getRotation()}deg)`
     container.appendChild(square1);
 
     const square2 = document.createElement('div');
@@ -98,6 +107,7 @@ const shuffle = () => {
     square2.style.height = square2Y + 'px';
     square2.style.left = getX(square2X) + 'px';
     square2.style.top = getY(square2Y) + 'px';
+    square2.style.transform = `rotate(${getRotation()}deg)`
     container.appendChild(square2);
 })();
 
